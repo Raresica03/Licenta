fea7703a5f6b5d42661f0f5d3b03f291fae1906c
Raresica <rares_padure12@yahoo.com>
2024-05-25 00:25:08 +0300

    created pages and navbar

---
+++ src/UIs/reactjs/src/pages/index.js
@@ -0,0 +1,13 @@
+import Home from "./Home/Home";
+import Welcome from "./Welcome/Welcome";
+import SignIn from "./SignIn/SignIn";
+import SignUp from "./SignUp/SignUp";
+import Account from "./Account/Account";
+
+export {
+    Home,
+    Welcome,
+    SignIn,
+    SignUp,
+    Account,
+}
